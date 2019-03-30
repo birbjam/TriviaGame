9afc50b3b8a0ba1a8933f499ae1db55b90de6707
birbjam <41635757+birbjam@users.noreply.github.com>
2019-03-30 07:45:05 -0700

    Start

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        prompt: "What color are apples?\n(a) Red/Green\n\(b) Purple\n(c) Orange",
+        answer: "a"
+    },
+    {
+        prompt: "What color are Bananas?\n(a) Teal\n\(b) Magenta\n(c) Yellow",
+        answer: "c"
+    },
+    {
+        prompt: "What color are strawberries?\n(a) Yellow\n\(b) Red\n(c) Blue",
+        answer: "a"
+    },
+];
+
+var score = 0;
