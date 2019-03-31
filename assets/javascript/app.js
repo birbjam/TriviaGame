bf8c6f84180853fa6c255bab842c3056fb7366de
birbjam <41635757+birbjam@users.noreply.github.com>
2019-03-31 13:18:51 -0700

    Add elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,5 @@
-var questions = [
-    {
-        prompt: "What color are apples?\n(a) Red/Green\n\(b) Purple\n(c) Orange",
-        answer: "a"
-    },
-    {
-        prompt: "What color are Bananas?\n(a) Teal\n\(b) Magenta\n(c) Yellow",
-        answer: "c"
-    },
-    {
-        prompt: "What color are strawberries?\n(a) Yellow\n\(b) Red\n(c) Blue",
-        answer: "a"
-    },
-];
+var score = "";
+var time = "";
 
-var score = 0;
 
-for(var i=0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-        score++;
-        alert("Correct!");
-    } else {
-        alert("WRONG!");
-    }
-}
-
-alert("you got " + score + "/" + questions.length);
+setTimeout(function () { alert("Hello"); }, 3000);
