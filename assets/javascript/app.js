18e32b7e6468865df7a9df39d968e8544f714c5c
birbjam <41635757+birbjam@users.noreply.github.com>
2019-04-01 10:53:20 -0700

    Added some functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,32 @@
-var score = "";
-var time = "";
+window.onload = function() {
+
+// Declaring global variables.
+var score = 0;
+var time = 0;
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var count = 0;
+
+// Waits for the start button to be pressed.
+    document.getElementById("startBtn").addEventListener("click", startGame);
+//When the start button is pressed, the div containing the questions appears in one second.
+    function startGame() {
+        setTimeout(function () { document.getElementById("questions").className="show"; }, 1000);
+    };
 
 
-setTimeout(function () { alert("Hello"); }, 3000);
+
+let answers = document.querySelectorAll('radio');
+
+for (let answer of answers) {
+    answer.addEventListener('click', function() {
+        var correct = this.dataset.type
+    }
+)};
+
+
+//document.getElementById("#timer").innerHTML("Time Left: " + time + "seconds");
+
+//document.getElementById("#score").innerHTML = "You got " + score + "/5 correct!";
+
+}
