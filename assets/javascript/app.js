4a756e4a489aa949ed93c7f133bfdc9b54414ad6
birbjam <41635757+birbjam@users.noreply.github.com>
2019-04-02 09:47:46 -0700

    Things that work: Radio buttons, showing a div after 1 second, the submit button. Things that don't work: literally everything else.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,16 @@
-window.onload = function() {
 
 // Declaring global variables.
 var score = 0;
-var time = 0;
 var correctAnswer = 0;
 var incorrectAnswer = 0;
-var count = 0;
+
+//Second attempt at timer, needs HTML modification
+function countDown(secs, elem) {
+    var element = document.getElementById(elem);
+    element.innerHTML = "Time Left: " + secs + "seconds";
+
+    secs--;
+}
 
 // Waits for the start button to be pressed.
     document.getElementById("startBtn").addEventListener("click", startGame);
@@ -16,17 +21,31 @@ var count = 0;
 
 
 
-let answers = document.querySelectorAll('radio');
+//Listens for a click on the submit button.
+    document.getElementById("submit").addEventListener("click", endGame);
 
-for (let answer of answers) {
-    answer.addEventListener('click', function() {
-        var correct = this.dataset.type
-    }
-)};
+//Shows the score screen after one second of clicking submit and displays the score.
+    function endGame() {
+        setTimeout(function () { document.getElementById("score").className="show"; }, 1000);
+        document.getElementById("score").innerHTML("Game Over!\nYou got " + score + "/5 correct!");
+    };
+
+
+
+
+// Testing a random timeout function.
+//    setTimeout(function () {
+//      alert("Done"); }, 30000);
+
+// Trying to select everything with the class 'radio'.
+//let answers = document.querySelectorAll('radio');
+
+//Loop that looks for a certain data type.
+//for (let answer of answers) {
+    //answer.addEventListener('click', function() {
+        //var correct = this.dataset.type
+    //}
+//)};
 
 
 //document.getElementById("#timer").innerHTML("Time Left: " + time + "seconds");
-
-//document.getElementById("#score").innerHTML = "You got " + score + "/5 correct!";
-
-}
