4c161bf640872467e40fd1bdb3a9a5dcc2652531
birbjam <41635757+birbjam@users.noreply.github.com>
2019-04-03 21:49:15 -0700

    Added: background, some CSS, questions and answers. Still broken: Clicking on the start button messes up the time, The score lingers in the background when you start the game again & the positioning of all the elements is wonky.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,14 @@
 //Global variable for score.
 var amountCorrect = 0;
 
+
 // Waits for the start button to be pressed.
     document.getElementById("startBtn").addEventListener("click", startGame);
 //When the start button is pressed, the div containing the questions appears in one second and the timer starts countdown.
     function startGame() {
         setTimeout(function () { document.getElementById("questions").className="show"; }, 1000);
 //Creates variable for how many seconds to start out with.
-        var timeleft = 10;
+        var timeleft = 20;
         var timer = setInterval(function () {
             document.getElementById("timer").innerHTML = "Time: " + timeleft + " seconds remaining";
 //Decrements time by one.
@@ -25,6 +26,7 @@ var amountCorrect = 0;
     };
 
 
+
 //Listens for a click on the submit button.
     document.getElementById("submit").addEventListener("click", endGame);
 
@@ -37,18 +39,16 @@ var amountCorrect = 0;
         document.getElementById("score").innerText = "Game Over! You got " + amountCorrect + "/5 correct!";
     };
 
-
 function handleClick() {
-
-//Loops through all the radio groups.
-for (var i = 1; i <= 45; i++) {
-    var radios = document.getElementsByName("group" + i);
-    for (var j = 0; j < radios.length; j++) {
-        var radio = radios[j];
-        //If correct radio button was selected, adds to the score counter.
-        if (radio.value === "correct" && radio.checked) {
-            amountCorrect++;
+    //Loops through all the radio groups.
+    for (var i = 1; i <= 45; i++) {
+        var radios = document.getElementsByName("group" + i);
+        for (var j = 0; j < radios.length; j++) {
+            var radio = radios[j];
+            //If correct radio button was selected, adds to the score counter.
+            if (radio.value === "correct" && radio.checked) {
+                amountCorrect++;
+            }
         }
     }
-}
 };
